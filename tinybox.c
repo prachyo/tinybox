8642858e3e815f7c93e37302e6a6ff348d9b560f
prachyo <prachyosarker565@gmail.com>
2026-03-11 18:25:35 -0400

    remove tracing on syscall exits

--- tinybox.c
+++ tinybox.c
@@ -36,23 +36,31 @@ int main(int argc, char *argv[]) {
 
         printf("tinybox: Monitoring started for PID %d\n", pid);
 
+        int on_enter = 1;
+
         while(1) {
             ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
             waitpid(pid, &status, 0);
 
             if (WIFEXITED(status) || WIFSIGNALED(status)) break;
 
-            struct user_regs_struct regs;
-            struct iovec iov;
-            iov.iov_base = &regs;
-            iov.iov_len = sizeof(regs);
+            if (WSTOPSIG(status) == (SIGTRAP | 0x80)) {
+                if (on_enter) {
+                    struct user_regs_struct regs;
+                    struct iovec iov;
+                    iov.iov_base = &regs;
+                    iov.iov_len = sizeof(regs);
+                    if (ptrace(PTRACE_GETREGSET, pid, (void*)NT_PRSTATUS, &iov) < 0) {
+                        perror("ptrace(GETREGSET)");
+                        break;
+                    }
+                    printf("tinybox: Syscall %llu\n", regs.orig_rax);
 
-            if (ptrace(PTRACE_GETREGSET, pid, (void*)NT_PRSTATUS, &iov) < 0) {
-                perror("ptrace(GETREGSET)");
-                break;
+                    on_enter = 0;
+                } else {
+                    on_enter = 1;
+                }
             }
-
-            printf("tinybox: Syscall %llu\n", regs.orig_rax);
         }
 
         printf("tinybox: Child finished execution.\n");
